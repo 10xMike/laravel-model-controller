8d88bd14d54b68700c3a60f7acd3aabbe90989a5
10xMike <michelegec@gmail.com>
2021-07-13 02:48:50 +0200

    Add .env copy, debug PageController.php

--- blog/app/Http/Controllers/PageController.php
+++ blog/app/Http/Controllers/PageController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Movie;
+
 class PageController extends Controller
 {
-    public function index(){
-        //restituire la view 'home'
-        return view('home');
+    public function index()
+    {
+        ddd( Movie::all());
+        return view('movies.index', compact('movies'));
+        
     }
 }
